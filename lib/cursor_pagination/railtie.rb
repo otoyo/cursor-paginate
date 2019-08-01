8c8e1872345df6f44af2239768704f24d720c066
otoyo <hiroki.t.1988@gmail.com>
2019-08-01 20:04:24 +0900

    Add require to railtie

--- lib/cursor_pagination/railtie.rb
+++ lib/cursor_pagination/railtie.rb
@@ -1,4 +1,9 @@
 module CursorPagination
   class Railtie < ::Rails::Railtie
+    initializer "cursor_pagination" do |app|
+      ActiveSupport.on_load :active_record do
+        require "cursor_pagination/active_record"
+      end
+    end
   end
 end
