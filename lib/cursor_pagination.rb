af6ba70feada5ed4a7e2686b5787e586674ad514
otoyo <hiroki.t.1988@gmail.com>
2019-08-01 13:32:29 +0900

    Fix require

--- lib/cursor_pagination.rb
+++ lib/cursor_pagination.rb
@@ -1,5 +1,8 @@
-require "cursor_pagination/railtie"
-
 module CursorPagination
-  # Your code goes here...
+end
+
+if defined?(Rails::Railtie)
+  require "cursor_pagination/railtie"
+else
+  raise "cursor_pagination is not available with your Rails version."
 end
